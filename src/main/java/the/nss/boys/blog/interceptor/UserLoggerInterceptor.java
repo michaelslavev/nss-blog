fe75093dcf37b43841379cdefdf2d066e5d0a667
Matěj Bárta <maguss.mdive@gmail.com>
2020-06-05 19:53:22 +0200

    Interceptor - javaDoc

--- src/main/java/the/nss/boys/blog/interceptor/UserLoggerInterceptor.java
+++ src/main/java/the/nss/boys/blog/interceptor/UserLoggerInterceptor.java
@@ -7,11 +7,19 @@ import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
 
-
+/**
+ * Interceptor for UserController
+ */
 public class UserLoggerInterceptor {
 
     private static final Logger LOG = LoggerFactory.getLogger(UserLoggerInterceptor.class);
 
+    /**
+     * Logs Before "method name" call and after "method name" call
+     * @param context method
+     * @return
+     * @throws Exception
+     */
     @AroundInvoke
     public Object log(InvocationContext context) throws Exception {
         LOG.debug("Before method " + context.getMethod().getName() + " call");
